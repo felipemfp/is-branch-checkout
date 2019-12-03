f5c8ebda7db99a280047bca886b024b31801c58a
Felipe Pontes <felipemfpontes@gmail.com>
2019-12-03 09:25:20 -0300

    Update to read HUSKY_GIT_PARAMS directly

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-const args = process.argv;
+const args = process.env.HUSKY_GIT_PARAMS
+  ? process.env.HUSKY_GIT_PARAMS.split(' ')
+  : process.argv;
 process.exit(args[args.length - 1] === '1' ? 0 : 1);
